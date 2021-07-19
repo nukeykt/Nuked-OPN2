8677f3e64c4736c9f6b46823fcde7ac02c206c81
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2021-07-19 16:03:54 +0300

    Update ym3438.h

--- ym3438.h
+++ ym3438.h
@@ -15,7 +15,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
- *
  *  Nuked OPN2(Yamaha YM3438) emulator.
  *  Thanks:
  *      Silicon Pr0n:
